1841a60e38e6c527d6f8d660d59d6462f28f5842
Alex <johan.higuita25@correo.tdea.edu.co>
2022-09-06 21:31:16 -0500

    Java project started

---
+++ Listas/.history/src/proyectolistas/ListaSimple_20220906203938.java
@@ -0,0 +1,14 @@
+
+package proyectolistas;
+
+import javax.swing.JOptionPane;
+
+public class ListaSimple {
+    private Nodo Cab;
+
+    public ListaSimple() {
+        Cab = null;
+    }
+    
+
+}
---
+++ Listas/.history/src/proyectolistas/ListaSimple_20220906210228.java
@@ -0,0 +1,92 @@
+
+package proyectolistas;
+
+import javax.swing.JOptionPane;
+
+public class ListaSimple {
+    private Nodo Cab;
+
+    public ListaSimple() {
+        Cab = null;
+    }
+
+    public void insertarInicio(int dato) {
+        Nodo nuevo = new Nodo(dato);
+        if (Cab == null) {
+            Cab = nuevo;
+        } else {
+            nuevo.setLiga(Cab);
+            Cab = nuevo;
+        }
+    }
+
+    public void insertarFinal(int dato) {
+        Nodo nuevo = new Nodo(dato);
+        if (Cab == null) {
+            Cab = nuevo;
+        } else {
+            Nodo aux = Cab;
+            while (aux.getLiga() != null) {
+                aux = aux.getLiga();
+            }
+            aux.setLiga(nuevo);
+        }
+    }
+
+    public void mostrar() {
+        Nodo aux = Cab;
+        String cad = "";
+        while (aux != null) {
+            cad = cad + aux.getDato() + " ";
+            aux = aux.getLiga();
+        }
+        JOptionPane.showMessageDialog(null, cad);
+    }
+
+    public void EliminarPrimerDato() {
+        if (Cab != null) {
+            Cab = Cab.getLiga();
+        }
+    }
+
+    public void EliminarUltimo() {
+        if (Cab != null) {
+            Nodo aux = Cab;
+            Nodo aux2 = Cab;
+            while (aux.getLiga() != null) {
+                aux2 = aux;
+                aux = aux.getLiga();
+            }
+            aux2.setLiga(null);
+        }
+    }
+
+    public void EliminarDato(int dato) {
+        if (Cab != null) {
+            Nodo aux = Cab;
+            Nodo aux2 = Cab;
+
+            while (aux.getDato() != dato) {
+                if (aux.getDato() == dato) {
+                    aux2.setLiga(aux.getLiga());
+                } else {
+                    aux2 = aux;
+                    aux = aux.getLiga();
+                }
+            }
+        }
+    }
+
+    // getters Cab
+    public Nodo getCab() {
+        return Cab;
+    }
+
+    // setters Cab
+    public void setCab(Nodo Cab) {
+        this.Cab = Cab;
+    }
+
+
+
+}
